2bb03ea89363cb7ecfdab0319bdeb951950bbb78
fsanche3 <fsanche3@oswego.edu>
2023-09-20 17:00:43 -0400

    init testing

--- MyCookBookPoller/tests/layers/service/spoonService.test.ts
+++ MyCookBookPoller/tests/layers/service/spoonService.test.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import {clearRecipes, pollForRecipes} from '../../../src/layers/service/spoonService';
+import {deleteRecipes} from '../../../src/layers/repository/recipeRepo';
+import {deleteIngredients} from '../../../src/layers/repository/ingredientRepo'
+
 
 jest.mock('axios');
+jest.mock('../../../src/layers/repository/recipeRepo');
+jest.mock('../../../src/layers/repository/ingredientRepo');
+
 
 const mockedAxios = axios as jest.MockedFunction<typeof axios>;
 
@@ -12,9 +18,20 @@ describe("Test for Spoon Service", () => {
         
     })
 
-    it("Should ", () => {
-        console.log("hello");
-        expect("hell").toEqual("hell")
+    it("Should invoke clear recipes and ingredients ", async () => {
+
+        // mockedAxios.mockResolvedValue({"records": 2});
+
+        const ingredientRepoSpy : jest.SpyInstance = jest.
+        spyOn({deleteIngredients}, 'deleteIngredients');
+
+        const recipeRepoSpy : jest.SpyInstance = jest.
+        spyOn({deleteRecipes}, 'deleteRecipes');
+
+        await clearRecipes();
+
+        expect(recipeRepoSpy).toBeCalledTimes(1);
+        expect(ingredientRepoSpy).toBeCalledTimes(1);
     })
 
     it("Should ", () => {
