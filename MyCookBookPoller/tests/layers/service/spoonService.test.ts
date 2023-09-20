c5480c64d2a54df98e0569ed626aa8b9838b0355
fsanche3 <fsanche3@oswego.edu>
2023-09-20 16:45:29 -0400

    folder restructuring

--- MyCookBookPoller/tests/layers/service/spoonService.test.ts
+++ MyCookBookPoller/tests/layers/service/spoonService.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {clearRecipes, pollForRecipes} from '../../../../src/layers/service/spoonService';
+import {clearRecipes, pollForRecipes} from '../../../src/layers/service/spoonService';
 
 jest.mock('axios');
 
